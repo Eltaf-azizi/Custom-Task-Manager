cd5d2c4d34c09aa2f91d086e81cd224d134aa1bd
Eltaf-azizi <iltfhuss@gmail.com>
2025-09-13 19:37:28 +0500

    working on the task

--- app/models.py
+++ app/models.py
@@ -13,3 +13,20 @@ class Category(db.Model):
 
     def __repr__(self):
         return f"<Category {self.name}>"
+
+
+
+class Task(db.Model):
+    __tablename__ = "task"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    deadline = db.Column(db.Date, nullable=True)
+    completed = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable=True)
+
+
+
+    def __repr__(self):
+        return f"<Task {self.title}>"
