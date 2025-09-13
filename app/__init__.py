175d3eef7a386dc2ea6ad56ddb7b3e533d7c8d10
Eltaf-azizi <iltfhuss@gmail.com>
2025-09-13 19:35:31 +0500

    working to ensure instance folder exists

--- app/__init__.py
+++ app/__init__.py
@@ -12,3 +12,23 @@ def create_app(config_class=Config):
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object(config_class)
 
+
+    # ensure instance folder exists
+    try:
+        os.makedirs(os.path.join(os.path.dirname(__file__), '..', 'instance'), exist_ok=True)
+    except OSError:
+        pass
+
+
+    db.init_app(app)
+
+
+    with app.app_context():
+        # import routes so they are registered
+        from . import routes, models
+        # create DB if it doesn't exist
+        db.create_all()
+
+
+    return app
+
